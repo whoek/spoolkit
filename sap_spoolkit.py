036ca1ad26017fa75859b32def9960ec6b49bcb5
HOEK Willem <hoekwi@nead.danet>
2017-09-06 12:12:21 +0100

    add simple template

--- sap_spoolkit.py
+++ sap_spoolkit.py
@@ -1,13 +1,19 @@
-print 'start'
+"""
 
-from flask import Flask
+SpoolKit
+
+
+"""
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World -- this is working !!!!"
+#    return "Hello World -- this is xx  working !!!!"
+    return render_template('index.html')
 
-
+# open up browser
 import webbrowser
 webbrowser.open('http://localhost:9090/', new = 2)
 
@@ -17,5 +23,4 @@ if __name__ == "__main__":
     app.run(port=9090)
 
 
-print 'done now' 
 
