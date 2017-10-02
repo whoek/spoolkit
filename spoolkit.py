bac75a84d9affefc2c2aedf989a76492364ee719
HOEK Willem <hoekwi@nead.danet>
2017-10-02 14:45:54 +0100

    navbar for index pages OK

--- spoolkit.py
+++ spoolkit.py
@@ -102,7 +102,7 @@ def shutdown():
 
 class SpoolkitSettings(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.Text)
+    key = db.Column(db.String(50))
     value = db.Column(db.Text)
 
 class SpoolkitReportgroups(db.Model):
