e0c5826685309bbcc4fd40f804fed82f08fbad58
HOEK Willem <hoekwi@nead.danet>
2017-10-05 13:27:56 +0100

    switch all to SQL Alchemy

--- spoolkit.py
+++ spoolkit.py
@@ -3,7 +3,8 @@
     :copyright: (c) 2017 by Willem Hoek.
 
 """
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, Markup
+import markdown
 import time
 import os
 import sys
@@ -53,46 +54,89 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
+def write_block(mode,block):
+    """
+    Used by insert tags 
+    """
+    if mode == '00':     # markdown
+        block = markdown.markdown(block)
+    elif mode == '01':   # sql
+
+
+    
+        block = '<h1>*** SQL ***</h1>'
+    else:   # this should NOT happen
+        pass  
+    return Markup(block)
+
+def insert_tags(text):
+    """
+    returns the SQL table & markup text of a block of text
+
+    mode 00 == Markdown  [DEFAULT]
+    mode 01 == sql       SQL table
+    mode 03 == snippit   [FUTURE USE]
+    """
+    mode = '00'     # default 
+    blankline_count = 0
+    block = ''
+    report = '' 
+    for line in text.split('\n'):
+        try:
+            line_no_spaces = str(line).replace(' ','').replace('\r','').lower()
+        except:
+            line_no_spaces = line           # weird encoding 
+        if line_no_spaces == '--sql':
+            report += write_block(mode, block)
+            mode = '01'
+            blankline_count = 0
+            block = ''
+        elif line_no_spaces == '' and mode == '01':
+            blankline_count += 1
+        else:
+            block += line + '\n'
+            blankline_count = 0 
+        if line_no_spaces == '--text' or blankline_count == 3:
+            report += write_block(mode, block)
+            mode = '00'
+            blankline_count = 0
+            block = ''        
+    report += write_block(mode, block)
+    return report
+
+############################### ROUTING ################################################
+
 @app.route("/")
 def root():
-    db = get_db()
-    c = db.execute('select * from spoolkit_reports')
-    reports = c.fetchall()
+    reports = SpoolkitReports.query.all()    
     return render_template('spoolkit_index.html',
             reports = reports)
 
 @app.route("/test")
 def test():
-    db = get_db()
-    c = db.execute('select * from spoolkit_reports')
-    reports = c.fetchall()
+    reports = SpoolkitReports.query.all()    
     return render_template('spoolkit_test.html',
             reports = reports)
 
 @app.route("/r/<int:id>")
 def view_report(id):
-    db = get_db()
-    sql = ('select * from spoolkit_reports where id = %s' % id)
-    c = db.execute(sql)
-    report = c.fetchone()
-    sql_result = db.execute(report['body_script']) 
+    report = SpoolkitReports.query.filter_by(id=id).first_or_404()
+    md_text = insert_tags(report.script)
     return render_template('spoolkit_report.html',
-            sql_result = sql_result,
-            report = report )
+            report = report,
+            md_text = md_text,
+            )
 
 @app.context_processor
 def inject_now():
     return {'now': time.ctime() }
 
-def shutdown_server():
+@app.route('/shutdown', methods=['GET'])   # GET = link,  POST = button
+def shutdown():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
-
-@app.route('/shutdown', methods=['GET'])   # GET = link,  POST = button
-def shutdown():
-    shutdown_server()
     return '''<br><br><br><br><h1>Thank you for using Spoolkit<br>
  Program shutting down... </h2><br>
         '''
@@ -111,14 +155,12 @@ class SpoolkitReportgroups(db.Model):
 
 class SpoolkitReports(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    active = db.Column(db.Boolean, default=True)
+    is_active = db.Column(db.Boolean, default=True)
     name = db.Column(db.String(60))
     shortcode = db.Column(db.String(10))
     report_group = db.Column(db.String(10))
     connection = db.Column(db.Text)
-    pre_script = db.Column(db.Text)
-    body_script = db.Column(db.Text)
-    post_script = db.Column(db.Text)
+    script = db.Column(db.UnicodeText)
     cache_duration = db.Column(db.Integer, default=0)
 
 class SpoolkitUsers(db.Model):
@@ -149,7 +191,7 @@ class SpoolkitSapfiles(db.Model):
 
 class SpoolkitConnections(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    active = db.Column(db.Boolean)
+    is_active = db.Column(db.Boolean)
     name = db.Column(db.Text)
     driver = db.Column(db.Text)
     host = db.Column(db.Text)
@@ -161,9 +203,9 @@ class SpoolkitConnections(db.Model):
 db.create_all()
 
 # Add some TEST entries
-rec1 = SpoolkitReports(name= 'Show date', body_script = 'select date()', shortcode = '1')
-rec2 = SpoolkitReports(name= 'Show list of reports', body_script = 'select * from spoolkit_reports', shortcode = 'M2')
-rec3 = SpoolkitReports(name= 'List of sap_files', body_script = 'select * from spoolkit_sapfiles',shortcode = 'K2')
+rec1 = SpoolkitReports(name= 'Show date', script = 'select date()', shortcode = '1', is_active = True)
+rec2 = SpoolkitReports(name= 'Show list of reports', script = 'select * from spoolkit_reports', shortcode = 'M2', is_active = True)
+rec3 = SpoolkitReports(name= 'List of sap_files', script = 'select * from spoolkit_sapfiles',shortcode = 'K2', is_active = True)
 db.session.add_all([rec1, rec2, rec3])
 db.session.commit()
 
@@ -178,11 +220,11 @@ class FileView(BaseView):
 #        return self.render('spoolkit_base.html')
 
 class ReportView(ModelView):
-    column_editable_list = ['active', 'name','shortcode']
+    column_editable_list = ['is_active','shortcode']
     column_display_pk = True
 
 class SapFileView(ModelView):
-#    column_editable_list = ['active', 'name']
+#    column_editable_list = ['is_active', 'name']
     column_display_pk = True
 
 #admin = Admin(app, name='Spoolkit', template_mode='bootstrap3', base_template='index2.html')
