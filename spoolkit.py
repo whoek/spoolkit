7a8a99b08b167323c8cb2c0fcd300f679eb80da6
HOEK Willem <hoekwi@nead.danet>
2017-10-19 14:26:03 +0100

    loading working !! need to test with EXE

--- spoolkit.py
+++ spoolkit.py
@@ -12,38 +12,39 @@ https://stackoverflow.com/questions/19516767/controlling-flask-server-from-comma
 
 """
 from flask import Flask, render_template, g, request, Markup, jsonify, flash, redirect, url_for
+from flask_admin import Admin, BaseView, expose
+from flask_admin.contrib.sqla import ModelView
+from flask_sqlalchemy import SQLAlchemy
 import markdown
-import time
-import timeit
 import os
 from os import listdir
 from os.path import isfile, join, getsize, getctime
-import sys
 import sqlite3
-import webbrowser
 import string
 import shutil
+import subprocess
+import sys
+import time
+import timeit
+import webbrowser
 
-from flask_admin import Admin, BaseView, expose
-from flask_sqlalchemy import SQLAlchemy
-from flask_admin.contrib.sqla import ModelView
-
-APP_PATH = os.path.abspath(".")          # application path
+os.sep = '/'      
+APP_PATH = os.path.abspath(".").replace('\\','/')          # application path
 
 if hasattr(sys, '_MEIPASS'):
     os.chdir(sys._MEIPASS)                 # change current working directory
 
 app = Flask(__name__)
 app.config.update(dict(
-    DATABASE=os.path.join(APP_PATH, 'sap_spoolkit.db'),
+    DATABASE=os.path.join(APP_PATH, 'sap_spoolkit.db').replace('\\','/'),
     SQLALCHEMY_DATABASE_URI='sqlite:///' +
-    os.path.join(APP_PATH, 'sap_spoolkit.db'),
+    os.path.join(APP_PATH, 'sap_spoolkit.db').replace('\\','/'),
     SQLALCHEMY_TRACK_MODIFICATIONS=True,
     SECRET_KEY='F34TF$($e34Q',
     USERNAME='admin',
     PASSWORD='default',
     APP_PATH=APP_PATH,
-    CWD_PATH=os.getcwd(),
+    CWD_PATH=os.getcwd().replace('\\','/'),
 ))
 
 db = SQLAlchemy(app)   # used by flask-admin
@@ -146,6 +147,10 @@ def insert_tags(text):
     return report, table_count
 
 
+###############################################################################
+# Load file
+###############################################################################
+
 def make_field_unique(f1, field):
     """ Make sure the field is not same as already used field
     If it is same make the field z1, z2,.....etc
@@ -164,6 +169,13 @@ def make_field_unique(f1, field):
                 unique = 0
     return field
 
+def create_sqlite_table(status):
+    sql = 'create table %s ( \n "' % (status["table_name"],)
+    sql += '"  text, \n "'.join(status["header_fields"])
+    sql += '"  text \n); \n\n'
+    return sql
+
+
 def get_fields(splitline):
     ft = []     # field_tuple, "(field1, field2, ...)"
     # build tuple of all fields
@@ -178,14 +190,13 @@ def get_fields(splitline):
         ft.append(string.strip(field))
     return tuple(ft)
 
-
-def ensure_dir(f):
+def ensure_dir(file_path):
     """
     http://stackoverflow.com/questions/273192/check-if-a-directory-exists-and-create-it-if-necessary
     """
-    d = os.path.dirname(f)
-    if not os.path.exists(d):
-        os.makedirs(d)
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def fileload_sqlite(fullfilename, sapfile_setup):
 
@@ -201,9 +212,14 @@ def fileload_sqlite(fullfilename, sapfile_setup):
     linenum = 0
 
     try:
-        input = file(os.path.abspath(fullfilename))
-        status["mypath"] = os.path.dirname(fullfilename)
-        status["myfile"] = os.path.basename(fullfilename)
+        input = file(os.path.abspath(fullfilename).replace('\\','/'))
+        status["mypath"] = os.path.dirname(fullfilename).replace('\\','/')
+        status["myfile"] = os.path.basename(fullfilename).replace('\\','/')
+
+        csvfile = APP_PATH + '/temp/' + status["myfile"]
+        ensure_dir(csvfile)
+        csv = open(csvfile, 'w')
+
         #f = codecs.open(os.path.abspath(input_file), "r",encoding='ascii')
         for line in input.readlines():
             linenum += 1
@@ -219,50 +235,80 @@ def fileload_sqlite(fullfilename, sapfile_setup):
 
             if step == GOT_HEADER:
                 splitline = string.splitfields(line,DELIMIT)
+                splitline_len = len(splitline)
+                if splitline_len < status["header_fields_count"]:
+                    missing = field_len - splitline_len
+                    splitline.extend(['' for i in missing*'-'])
+                s = "\t".join(str(x).strip(' \t\n') for x in splitline[0:field_len]) + '\n'
+                csv.write(s)
+
             elif step == START:
                 for sapfile in sapfile_setup:
-                    if sapfile["keyword"].lower() in line.lower():     # found KEYWORD                        
+                    if sapfile.keyword.lower() in line.lower():     # found KEYWORD                        
                         status["linenum_keyword"] = linenum
-                        status["keyword"] = sapfile["keyword"].lower()
+                        status["keyword"] = sapfile.keyword.lower()
                         step = GOT_KEY
                     elif linenum > MAX_SEARCH:
                         break
 
             elif step == GOT_KEY:
                 for sapfile in sapfile_setup:
-                    if sapfile["header_field"].lower() in line.lower():
+                    if sapfile.header_field.lower() in line.lower():
                         # Found HEADER
-                        status["header_field"] = sapfile["header_field"].lower()
+                        status["header_field"] = sapfile.header_field.lower()
                         status["linenum_header_field"] = linenum
-                        status["table_name"] = sapfile["table_name"].lower()
+                        status["table_name"] = sapfile.table_name.lower()
                         splitline = string.splitfields(line,DELIMIT)
                         status["header_fields_init"] = splitline
                         status["header_fields"] = get_fields(splitline)
                         status["header_fields_count"] = len(status["header_fields"])
+                        field_len = status["header_fields_count"]
                         s = "\t".join(str(x).strip(' \t\n') for x in status["header_fields"]) + '\n'              
                         status["s"] = s
+                        step = GOT_HEADER
 
                     elif linenum > MAX_SEARCH:
                         break
 
-        input.close()
         status["linenum_end"] = linenum
+        input.close()
+        csv.close()
+        
+        if "header_fields_count" in status:
+            sql_drop = "drop table if exists %s;\n\n" % (status["table_name"],)
+            sql_create = create_sqlite_table(status)
+            sql_import = ".import {}  {} ".format(csvfile, status["table_name"])
+            
+            # subprocess starting now
+            sql_result = subprocess.call(["sqlite3.exe",  "sap_spoolkit.db", 
+                sql_drop, sql_create, ".separator '\t'",sql_import])  
 
-        # create SQL and TXT file
+            status["sql_result"] = sql_result
+            status["sql_drop"] = sql_drop
+            status["sql_create"] = sql_create
+            status["sql_import"] = sql_import
+            
+            try:
+                os.remove(csvfile)
+            except:
+                pass    #else delete it when program start up again
 
-        # move the file to archive
-        to_dir = status["mypath"] + '/archive/' + time.strftime('%Y_%m_%d') + '/'
-        ensure_dir(to_dir)
-    #    shutil.move(fullfilename, to_dir + '/' + str(status["myfile"]))
-        shutil.copy(fullfilename, to_dir + '/' + str(status["myfile"]))
+            if sql_result == 0:
+                #when done, move the TXT file to archive
+                to_file = status["mypath"] + '/archive/' + time.strftime('%Y_%m_%d') + \
+                    '/' + str(status["myfile"])
+                ensure_dir(to_file)
+                shutil.move(fullfilename, to_file)
+#                shutil.copy(fullfilename, to_file)
 
     # write summary in a log file and return ID
 
     except IOError as e:
-        print("({})".format(e))
+        status["error"] = "({})".format(e)
 
     status["duration"] = str(timeit.default_timer() - start_time)
     return status
+
 
 #######################################################################################
 #
@@ -324,7 +370,7 @@ def loadfiles():
         for f in file_list:
             t_file = {}
             t_file["filename"] = f
-            t_file["fullfilename"] = str(os.path.join(mypath, f))
+            t_file["fullfilename"] = str(os.path.join(mypath, f).replace('\\','/'))
             t_file["filedate"] = str(time.ctime(getctime(mypath + '/' + f)))
             t_file["filesize"] = str(os.path.getsize(mypath + '/' + f)/1000000)
 
@@ -356,19 +402,12 @@ def loadfiles():
 @app.route('/file_process', methods=['GET', 'POST'])
 def file_process():
     if request.method == 'POST':
-#        time.sleep(1)
-#   DELETE THIS WHEN DONE #TODO
-#   NEED TO PULL FROM DATABASE
-        sapfile_setup = [{"keyword": 'willem', "header_field": 'zzzz', "table_name": 'willem_zzzz'},
-            {"keyword": 'willem', "header_field": 'field', "table_name": 'willem_field'},
-            {"keyword": 'willem', "header_field": 'zzz', "table_name": 'willem_zzz'},
-            ]
-
+        sapfile_setup = SpoolkitSapfiles.query.all()
         fullfilename  = request.form.get("fullfilename")
         if fullfilename is None:
             flash('No file was selected')
         else:
-            status  = fileload_sqlite(fullfilename, sapfile_setup)
+            status = fileload_sqlite(fullfilename, sapfile_setup)
             flash(str(status))
         return redirect(url_for('loadfiles'))
     else:
--- test.py
+++ test.py
@@ -2,11 +2,12 @@ import timeit
 import os
 import string
 import time
+import datetime
 import shutil
 from os import listdir
 from os.path import isfile, join, getsize, getctime
 import sys
-
+import subprocess
 
 def make_field_unique(f1, field):
     """ Make sure the field is not same as already used field
@@ -26,6 +27,13 @@ def make_field_unique(f1, field):
                 unique = 0
     return field
 
+def create_sqlite_table(status):
+    sql = 'create table %s ( \n "' % (status["table_name"],)
+    sql += '"  text, \n "'.join(status["header_fields"])
+    sql += '"  text \n); \n\n'
+    return sql
+
+
 def get_fields(splitline):
     ft = []     # field_tuple, "(field1, field2, ...)"
     # build tuple of all fields
@@ -40,13 +48,13 @@ def get_fields(splitline):
         ft.append(string.strip(field))
     return tuple(ft)
 
-def ensure_dir(f):
+def ensure_dir(file_path):
     """
     http://stackoverflow.com/questions/273192/check-if-a-directory-exists-and-create-it-if-necessary
     """
-    d = os.path.dirname(f)
-    if not os.path.exists(d):
-        os.makedirs(d)
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def fileload_sqlite(fullfilename, sapfile_setup):
 
@@ -65,6 +73,11 @@ def fileload_sqlite(fullfilename, sapfile_setup):
         input = file(os.path.abspath(fullfilename))
         status["mypath"] = os.path.dirname(fullfilename)
         status["myfile"] = os.path.basename(fullfilename)
+
+        csvfile = APP_PATH + '/temp/' + status["myfile"]
+        ensure_dir(csvfile)
+        csv = open(csvfile, 'w')
+
         #f = codecs.open(os.path.abspath(input_file), "r",encoding='ascii')
         for line in input.readlines():
             linenum += 1
@@ -80,6 +93,13 @@ def fileload_sqlite(fullfilename, sapfile_setup):
 
             if step == GOT_HEADER:
                 splitline = string.splitfields(line,DELIMIT)
+                splitline_len = len(splitline)
+                if splitline_len < status["header_fields_count"]:
+                    missing = field_len - splitline_len
+                    splitline.extend(['' for i in missing*'-'])
+                s = "\t".join(str(x).strip(' \t\n') for x in splitline[0:field_len]) + '\n'
+                csv.write(s)
+
             elif step == START:
                 for sapfile in sapfile_setup:
                     if sapfile["keyword"].lower() in line.lower():     # found KEYWORD                        
@@ -100,27 +120,49 @@ def fileload_sqlite(fullfilename, sapfile_setup):
                         status["header_fields_init"] = splitline
                         status["header_fields"] = get_fields(splitline)
                         status["header_fields_count"] = len(status["header_fields"])
+                        field_len = status["header_fields_count"]
                         s = "\t".join(str(x).strip(' \t\n') for x in status["header_fields"]) + '\n'              
                         status["s"] = s
+                        step = GOT_HEADER
 
                     elif linenum > MAX_SEARCH:
                         break
 
-        input.close()
         status["linenum_end"] = linenum
+        input.close()
+        csv.close()
+        
+        if "header_fields_count" in status:
+            sql_drop = "drop table if exists %s;\n\n" % (status["table_name"],)
+            sql_create = create_sqlite_table(status)
+            sql_import = ".import {}  {} ".format(csvfile, status["table_name"])
+            
+            # subprocess starting now
+            result = subprocess.call(["sqlite3.exe",  "sap_spoolkit.db", 
+                sql_drop, sql_create, ".separator '\t'",sql_import])  
 
-        # create SQL and TXT file
+            # delete the CSV file in temp directory
+            try:
+                os.remove(csvfile)
+            except:
+                pass    #else delete it when program start up again
+#            print 'sql_drop: ', sql_drop
+#            print 'sql_create: ', sql_create
+#            print 'sql_import:', sql_import
+#            print 'result = ', result
 
-        # move the file to archive
-        to_dir = status["mypath"] + '/archive/' + time.strftime('%Y_%m_%d') + '/'
-        ensure_dir(to_dir)
-    #    shutil.move(fullfilename, to_dir + '/' + str(status["myfile"]))
-        shutil.copy(fullfilename, to_dir + '/' + str(status["myfile"]))
+            if result == 0:
+                #when done, move the TXT file to archive
+                to_file = status["mypath"] + '/archive/' + time.strftime('%Y_%m_%d') + \
+                    '/' + str(status["myfile"])
+                ensure_dir(to_file)
+                shutil.move(fullfilename, to_file)
+#                shutil.copy(fullfilename, to_file)
 
     # write summary in a log file and return ID
 
     except IOError as e:
-        print("({})".format(e))
+        status["error"] = "({})".format(e)
 
     status["duration"] = str(timeit.default_timer() - start_time)
     return status
@@ -130,15 +172,19 @@ def fileload_sqlite(fullfilename, sapfile_setup):
 # MAIN
 ########################################################################
 
-sapfile_setup = [{"keyword": 'willem', "header_field": 'zzzz', "table_name": 'willem_zzzz'},
-    {"keyword": 'willem', "header_field": 'field', "table_name": 'willem_field'},
-    {"keyword": 'willem', "header_field": 'zzz', "table_name": 'willem_zzz'},
+sapfile_setup = [{"keyword": 'willem', "header_field": 'zzzz', "table_name": 't1'},
+    {"keyword": 'willem', "header_field": 'test', "table_name": 'w1'},
+    {"keyword": 'willem', "header_field": 'zzz', "table_name": 't2'},
     ]
 
-mypath = 'C:\junk\_test'
+
+mypath = 'C:/junk/_sapfiles'            # where to find the TXT files
+APP_PATH = 'C:/junk/_test'          # where to find the DB & /temp/ directory
+
 file_list = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 for txtfile in file_list:
-    status = fileload_sqlite( mypath + '/' + txtfile, sapfile_setup)
+    file2load = os.path.join(mypath, txtfile)
+    status = fileload_sqlite(file2load, sapfile_setup)
     print  status
     sys.stdout.flush()
 
